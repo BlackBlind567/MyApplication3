144e26172f11a433682cde82725107d24c1db37d
BlackBlind <shantanujaiswal20@gmail.com>
2018-07-06 18:50:39 -0700

    grid View working

--- app/src/main/java/com/example/blackblind/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/blackblind/myapplication3/MainActivity.java
@@ -1,31 +1,119 @@
 package com.example.blackblind.myapplication3;
 
+import android.content.Context;
 import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import javax.security.auth.callback.Callback;
+
+import static com.example.blackblind.myapplication3.imageUrl.IMAGES;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView gridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = findViewById(R.id.tv_search);
 
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,SearchActivity.class));
+        gridView = (GridView)findViewById(R.id.grid);
+        gridView.setAdapter(new ImageAdapter(this));
+    }
+
+    private static class ImageAdapter extends BaseAdapter {
+
+        private static final String[] IMAGE_URLS = imageUrl.IMAGES;
+
+        private LayoutInflater inflater;
+
+        Context c;
+
+        ImageAdapter(Context context) {
+            inflater = LayoutInflater.from(context);
+            c = context;
+
+        }
+
+        @Override
+        public int getCount() {
+            return IMAGE_URLS.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+            View view = convertView;
+            if (view == null) {
+                view = inflater.inflate(R.layout.item_grid_image, parent, false);
+                holder = new ViewHolder();
+                assert view != null;
+
+                holder.imageView = (ImageView) view.findViewById(R.id.image);
+
+                holder.progressBar = (ProgressBar) view.findViewById(R.id.progress);
+
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
             }
-        });
+            Picasso.with(c)
+                    .load(IMAGE_URLS[position])
+                    .placeholder(R.drawable.ic_action_navigation_arrow_back)
+                    .error(R.drawable.ic_action_navigation_arrow_back_inverted)
+                    .fit()
+                    .into(holder.imageView, new com.squareup.picasso.Callback() {
 
+                        @Override
+                        public void onSuccess() {
+                            holder.imageView.setVisibility(View.VISIBLE);
+                            holder.progressBar.setVisibility(View.INVISIBLE);
+                        }
 
+                        @Override
+                        public void onError() {
+                            holder.progressBar.setVisibility(View.VISIBLE);
+                            holder.imageView.setVisibility(View.INVISIBLE);
+                        }
+                    });
 
-//        Picasso.with(getApplicationContext()).load("https://blackcblind.000webhostapp.com/slide3.png").into(imageView);
+            return view;
+        }
+    }
+
+    static class ViewHolder {
+        ImageView imageView;
+        ProgressBar progressBar;
     }
 }
+
+
---
+++ app/src/main/java/com/example/blackblind/myapplication3/imageUrl.java
@@ -0,0 +1,14 @@
+package com.example.blackblind.myapplication3;
+
+public final class imageUrl {
+
+    public static final String[] IMAGES = new String[] {
+// Heavy images
+            "https://blackcblind.000webhostapp.com/a.JPG",
+            "https://blackcblind.000webhostapp.com/a.JPG",
+            "https://blackcblind.000webhostapp.com/a.JPG",
+            "https://blackcblind.000webhostapp.com/a.JPG",
+
+
+    };
+}
